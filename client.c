3372b153601c40c6d325ce547b87233b549ea533
Nam Anh <namanh@Nams-Air.lan>
2024-11-07 18:37:39 +0700

    session + online users list

--- client.c
+++ client.c
@@ -3,12 +3,45 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 #include "protocol.c"
 
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 8080
 
 int is_logged_in = 0;
+
+// Function to continuously receive messages from the server
+void *receive_messages(void *arg) {
+    int sockfd = *(int *)arg;
+    Message msg;
+
+    while (1) {
+        if (receive_message(sockfd, &msg) <= 0) {
+            printf("Server disconnected\n");
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+
+        // Process received message
+        switch (msg.type) {
+            case RESP_SUCCESS:
+                printf("Login successful!\n");
+                is_logged_in = 1;
+                break;
+            case RESP_FAILURE:
+                printf("Login or registration failed.\n");
+                break;
+            case MSG_ONLINE_USERS:
+                printf("Online users: %s\n", (char *)msg.payload);
+                break;
+            default:
+                printf("Unknown message type received.\n");
+                break;
+        }
+    }
+    return NULL;
+}
 
 // Hàm kết nối đến server
 int connect_to_server() {
@@ -73,13 +106,10 @@ void login_user(int sockfd) {
     // Create the payload in the format "username:password"
     char payload[256];
     snprintf(payload, sizeof(payload), "%s:%s", username, password);
-    
-    // Print the payload for debugging
-    printf("Payload for login: '%s'\n", payload);
 
     // Create the login message and print its contents
     Message msg = create_message(MSG_LOGIN, (uint8_t *)payload, strlen(payload));
-    printf("Message type: %d, Message payload: '%s'\n", msg.type, (char *)msg.payload);
+    // printf("Message type: %d, Message payload: '%s'\n", msg.type, (char *)msg.payload);
 
     // Send the message to the server and check for errors
     if (send_message(sockfd, &msg) < 0) {
@@ -92,19 +122,39 @@ void login_user(int sockfd) {
 
 int main() {
     int sockfd = connect_to_server();
-    int command;
+    pthread_t recv_thread;
 
+    // Create a thread to receive messages from the server
+    if (pthread_create(&recv_thread, NULL, receive_messages, &sockfd) != 0) {
+        perror("Failed to create receive thread");
+        close(sockfd);
+        return EXIT_FAILURE;
+    }
+
+    int command;
     while (1) {
-        printf("Enter command (1: register, 2: login, 3: send message, 4: exit): ");
+        if(is_logged_in) {
+            printf("Enter command (3: send message, 4: exit): ");
+        } else {
+            printf("Enter command (1: register, 2: login, 3: send message, 4: exit): ");
+        }
         scanf("%d", &command);
         // Xóa bộ đệm nhập liệu
         while (getchar() != '\n'); // Xóa tất cả ký tự dư trong buffer cho đến khi gặp '\n'
 
         switch (command) {
             case 1:
+                if(is_logged_in) {
+                    printf("You must logout first\n");
+                    break;
+                }
                 register_user(sockfd);
                 break;
             case 2:
+                if(is_logged_in) {
+                    printf("You are already logged in\n");
+                    break;
+                }
                 login_user(sockfd);
                 break;
             case 3:
